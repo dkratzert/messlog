26c0f406491b582684b1446eda5c8160b1004ed9
Daniel Kratzert <dkratzert@gmx.de>
2018-11-27 13:23:58 +0100

    tests for views

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,13 @@
 from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+
 from django.urls import reverse
 from django.views import generic
 
 from polls.models import Choice, Question
+from django.utils import timezone
+
 
 # Create your views here.
 
@@ -14,12 +17,18 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return [x for x in Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date') if x.choice_set.all()][:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -34,9 +43,9 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
-                     'question': question,
-                     'error_message': "You didn't select a choice.",
-                })
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
     else:
         selected_choice.votes = F('votes') + 1
         selected_choice.save()
