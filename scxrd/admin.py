7043fec11f7d7d7e0f44fbcd2edf4a06667eeabb
Daniel Kratzert <dkratzert@gmx.de>
2018-12-02 19:36:21 +0100

    solvents work

--- scxrd/admin.py
+++ scxrd/admin.py
@@ -8,20 +8,20 @@ from .models import Experiment, Machine, Solvent
 
 
 class SolventInline(admin.TabularInline):
-    model = Solvent
+    model = Experiment.solvents_used.through
 
 
 class ExperimentAdmin(admin.ModelAdmin):
-    list_display = ('experiment', 'number', 'date', 'machine', 'solvents_used', 'sum_formula')
+    list_display = ('experiment', 'number', 'date', 'machine', 'sum_formula')
     list_filter = ['date']
     search_fields = ['experiment', 'number', 'sum_formula']
-    empty_value_display = 'unknown'
-    #inlines = [SolventInline, ]
-    formfield_overrides = {
-        Solvent: {'widget': MultipleChoiceField},
-    }
+    ordering = ['number']
+    #empty_value_display = 'unknown'
+    #formfield_overrides = {
+    #    Solvent: {'widget': MultipleChoiceField},
+    #}
 
 
 admin.site.register(Experiment, ExperimentAdmin)
-#admin.site.register(Machine)
-#admin.site.register(Solvent)
+admin.site.register(Machine)
+admin.site.register(Solvent)
--- scxrd/models.py
+++ scxrd/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -14,24 +15,27 @@ class Machine(models.Model):
 
 
 class Solvent(models.Model):
-    name = models.CharField(verbose_name="solvents name", max_length=200)
+    name = models.CharField(verbose_name="solvents name", max_length=200, unique=True)
 
     def __str__(self):
         return self.name
 
 
 # Editable lists of solvents and machines for the experiment:
-SOLVENT_NAMES = [(x.pk, x.__str__()) for x in Solvent.objects.all()]
+#SOLVENT_NAMES = [(x.pk, x.__str__()) for x in Solvent.objects.all()]
 MACHINE_NAMES = [(x.pk, x.__str__()) for x in Machine.objects.all()]
 
 
 class Experiment(models.Model):
     experiment = models.CharField(verbose_name='experiment name', max_length=200, blank=False, default=None)
+    number = models.IntegerField(verbose_name='number', unique=True, validators=[MinValueValidator(1)])
     date = models.DateTimeField(verbose_name='time of measurement', default=timezone.now)
-    number = models.IntegerField(verbose_name='number', unique=True)
-    sum_formula = models.CharField(max_length=300)
-    solvents_used = models.IntegerField(verbose_name='solvents used', choices=SOLVENT_NAMES)
+    sum_formula = models.CharField(max_length=300, blank=True)
+    solvents_used = models.ManyToManyField(Solvent, verbose_name='solvents used', blank=True)
     machine = models.IntegerField(verbose_name='machine used', choices=MACHINE_NAMES)
+
+    class Meta:
+        ordering = ["number"]
 
     def was_measured_recently(self) -> bool:
         now = timezone.now()
