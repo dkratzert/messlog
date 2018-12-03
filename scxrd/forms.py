82f12771d2ebb5f0bb9adb7cc6027bfa6edcef28
Daniel Kratzert <dkratzert@gmx.de>
2018-12-03 13:07:47 +0100

    sdr

--- scxrd/forms.py
+++ scxrd/forms.py
@@ -8,11 +8,18 @@ from scxrd.models import Experiment
 class ExperimentForm(forms.ModelForm):
     class Meta:
         model = Experiment
+        measure_date = forms.DateField(
+            widget=forms.TextInput(
+                attrs={'type': 'date'}
+            )
+        )
         fields = ('experiment', 'number', 'measure_date', 'machine', 'sum_formula')
+        
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Save Experiment'))
+
 
