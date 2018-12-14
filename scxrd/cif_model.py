667ba819b57fc26a8f990fed34679f0f3d474194
Daniel Kratzert <dkratzert@gmx.de>
2018-12-14 18:59:09 +0100

    more cif fixes

--- scxrd/cif_model.py
+++ scxrd/cif_model.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.utils import timezone
+
 from scxrd.cif.cifparser import Cif
 from .utils import generate_sha256
+
+
+def get_float(line: str) -> float:
+    return float(line.split('(')[0].split(' ')[0])
+
+def get_int(line: str) -> int:
+    return int(line.split('(')[0].split(' ')[0])
 
 
 class CifFile(models.Model):
@@ -14,6 +22,14 @@ class CifFile(models.Model):
     date_updated = models.DateTimeField(verbose_name='change date', null=True, blank=True)
     filesize = models.PositiveIntegerField(null=True, blank=True)
     #########################################
+    data = models.CharField(null=True, blank=True, max_length=256)
+    _cell_length_a = models.FloatField(null=True, blank=True)
+    _cell_length_b = models.FloatField(null=True, blank=True)
+    _cell_length_c = models.FloatField(null=True, blank=True)
+    _cell_angle_alpha = models.FloatField(null=True, blank=True)
+    _cell_angle_beta = models.FloatField(null=True, blank=True)
+    _cell_angle_gamma = models.FloatField(null=True, blank=True)
+    _cell_volume = models.FloatField(null=True, blank=True)
     _cell_formula_units_Z = models.PositiveIntegerField(null=True, blank=True)
     _space_group_name_H_M_alt = models.CharField(max_length=255, null=True, blank=True)
     _space_group_name_Hall = models.CharField(max_length=255, null=True, blank=True)
@@ -31,9 +47,9 @@ class CifFile(models.Model):
     _exptl_crystal_size_min = models.FloatField(null=True, blank=True)
     _exptl_absorpt_coefficient_mu = models.FloatField(null=True, blank=True)
     _exptl_absorpt_correction_type = models.CharField(max_length=255, null=True, blank=True)
-    _diffrn_ambient_temperature  = models.FloatField(null=True, blank=True)
-    _diffrn_radiation_wavelength  = models.FloatField(null=True, blank=True)
-    _diffrn_radiation_type  = models.CharField(max_length=255, null=True, blank=True)
+    _diffrn_ambient_temperature = models.FloatField(null=True, blank=True)
+    _diffrn_radiation_wavelength = models.FloatField(null=True, blank=True)
+    _diffrn_radiation_type = models.CharField(max_length=255, null=True, blank=True)
     _diffrn_source = models.CharField(max_length=255, null=True, blank=True)
     _diffrn_measurement_device_type = models.CharField(max_length=255, null=True, blank=True)
     _diffrn_reflns_number = models.IntegerField(null=True, blank=True)
@@ -47,7 +63,7 @@ class CifFile(models.Model):
     _reflns_number_gt = models.PositiveIntegerField(null=True, blank=True)
     _reflns_threshold_expression = models.CharField(max_length=255, null=True, blank=True)
     _reflns_Friedel_coverage = models.FloatField(null=True, blank=True)
-    _computing_structure_solution  = models.CharField(max_length=255, null=True, blank=True)
+    _computing_structure_solution = models.CharField(max_length=255, null=True, blank=True)
     _computing_structure_refinement = models.CharField(max_length=255, null=True, blank=True)
     _refine_special_details = models.TextField(null=True, blank=True)
     _refine_ls_abs_structure_Flack = models.CharField(max_length=255, null=True, blank=True)
@@ -69,21 +85,25 @@ class CifFile(models.Model):
     _diffrn_reflns_av_unetI_netI = models.FloatField(null=True, blank=True)
     _database_code_depnum_ccdc_archive = models.CharField(max_length=255, null=True, blank=True)
     _shelx_res_file = models.TextField(max_length=255, null=True, blank=True)
+
     #################################
 
-    def save(self, *args, **kwargs):
+    def save_base(self, raw=False, force_insert=False,
+                  force_update=False, using=None, update_fields=None):
         super(CifFile, self).save(*args, **kwargs)
         with open(self.cif.file.name, encoding='ascii', errors='ignore') as cf:
             self.fill_residuals_table(cf.readlines())
         checksum = generate_sha256(self.cif.file)
+        print(checksum)
         self.filesize = self.cif.size
         # TODO: Make check if file exists work:
-        #inst = CifFile.objects.filter(sha1=checksum).first()
-        #if inst:
+        # inst = CifFile.objects.filter(sha1=checksum).first()
+        # if inst:
         #    self.cif = inst
         #    return
+        # https://timonweb.com/posts/cleanup-files-and-images-on-model-delete-in-django/
         self.sha1 = checksum
-        if not self.id:
+        if not self.date_created:
             self.date_created = timezone.now()
         self.date_updated = timezone.now()
         super(CifFile, self).save(*args, **kwargs)
@@ -105,144 +125,75 @@ class CifFile(models.Model):
         """
         Fill the table with residuals of the refinement.
         """
-        #if cif.cif_data['calculated_formula_sum']:
+        # if cif.cif_data['calculated_formula_sum']:
         #    self.fill_formula(structure_id, cif.cif_data['calculated_formula_sum'])
         cif = Cif()
         cifok = cif.parsefile(fullpath)
         print('file parsed')
 
-        cif_keys = [
-                        "data",
-                        "_cell_length_a",
-                        '_cell_length_b',
-                        '_cell_length_c',
-                        '_cell_angle_alpha',
-                        '_cell_angle_beta'
-                        '_cell_angle_gamma'
-                        "_cell_volume"
-                        "_cell_formula_units_Z",
-                        "_space_group_name_H-M_alt",
-                        "_space_group_name_Hall",
-                        "_space_group_centring_type",
-                        "_space_group_IT_number",
-                        "_space_group_crystal_system",
-                        "_space_group_symop_operation_xyz",
-                        "_audit_creation_method",
-                        "_chemical_formula_sum",
-                        "_chemical_formula_weight",
-                        "_exptl_crystal_description",
-                        "_exptl_crystal_colour",
-                        "_exptl_crystal_size_max",
-                        "_exptl_crystal_size_mid",
-                        "_exptl_crystal_size_min",
-                        "_exptl_absorpt_coefficient_mu",
-                        "_exptl_absorpt_correction_type",
-                        "_diffrn_ambient_temperature",
-                        "_diffrn_radiation_wavelength",
-                        "_diffrn_radiation_type",
-                        "_diffrn_source",
-                        "_diffrn_measurement_device_type",
-                        "_diffrn_reflns_number",
-                        "_diffrn_reflns_av_R_equivalents",
-                        "_diffrn_reflns_av_unetI/netI",
-                        "_diffrn_reflns_theta_min",
-                        "_diffrn_reflns_theta_max",
-                        "_diffrn_reflns_theta_full",
-                        "_diffrn_measured_fraction_theta_max",
-                        "_diffrn_measured_fraction_theta_full",
-                        "_reflns_number_total",
-                        "_reflns_number_gt",
-                        "_reflns_threshold_expression",
-                        "_reflns_Friedel_coverage",
-                        "_computing_structure_solution",
-                        "_computing_structure_refinement",
-                        "_refine_special_details",
-                        "_refine_ls_abs_structure_Flack",
-                        "_refine_ls_structure_factor_coef",
-                        "_refine_ls_weighting_details",
-                        "_refine_ls_number_reflns",
-                        "_refine_ls_number_parameters",
-                        "_refine_ls_number_restraints",
-                        "_refine_ls_R_factor_all",
-                        "_refine_ls_R_factor_gt",
-                        "_refine_ls_wR_factor_ref",
-                        "_refine_ls_wR_factor_gt",
-                        "_refine_ls_goodness_of_fit_ref",
-                        "_refine_ls_restrained_S_all",
-                        "_refine_ls_shift/su_max",
-                        "_refine_ls_shift/su_mean",
-                        "_refine_diff_density_max",
-                        "_refine_diff_density_min",
-                        "_diffrn_reflns_av_unetI_netI",
-                        "_database_code_depnum_ccdc_archive",
-                        "_shelx_res_file",
-                    ]
-        db_columns = [
-                    self._cell_formula_units_Z,                 
-                    self._space_group_name_H_M_alt,             
-                    self._space_group_name_Hall,                
-                    self._space_group_centring_type,            
-                    self._space_group_IT_number,                
-                    self._space_group_crystal_system,           
-                    self._space_group_symop_operation_xyz,      
-                    self._chemical_formula_sum,                 
-                    self._chemical_formula_weight,              
-                    self._exptl_crystal_description,            
-                    self._exptl_crystal_colour,                 
-                    self._exptl_crystal_size_max,               
-                    self._exptl_crystal_size_mid,               
-                    self._exptl_crystal_size_min,               
-                    self._audit_creation_method,                
-                    self._exptl_absorpt_coefficient_mu,         
-                    self._exptl_absorpt_correction_type,        
-                    self._diffrn_ambient_temperature,           
-                    self._diffrn_radiation_wavelength,          
-                    self._diffrn_radiation_type,                
-                    self._diffrn_source,                        
-                    self._diffrn_measurement_device_type,       
-                    self._diffrn_reflns_number,                 
-                    self._diffrn_reflns_av_R_equivalents,       
-                    self._diffrn_reflns_theta_min,              
-                    self._diffrn_reflns_theta_max,              
-                    self._diffrn_reflns_theta_full,             
-                    self._diffrn_measured_fraction_theta_max,   
-                    self._diffrn_measured_fraction_theta_full,  
-                    self._reflns_number_total,                  
-                    self._reflns_number_gt,                                                                            
-                    self._reflns_threshold_expression,          
-                    self._reflns_Friedel_coverage,                          
-                    self._computing_structure_solution,         
-                    self._computing_structure_refinement,       
-                    self._refine_special_details,               
-                    self._refine_ls_abs_structure_Flack,        
-                    self._refine_ls_structure_factor_coef,      
-                    self._refine_ls_weighting_details,          
-                    self._refine_ls_number_reflns,              
-                    self._refine_ls_number_parameters,          
-                    self._refine_ls_number_restraints,          
-                    self._refine_ls_R_factor_all,               
-                    self._refine_ls_R_factor_gt,                
-                    self._refine_ls_wR_factor_ref,              
-                    self._refine_ls_wR_factor_gt,               
-                    self._refine_ls_goodness_of_fit_ref,        
-                    self._refine_ls_restrained_S_all,                                  
-                    self._refine_ls_shift_su_max,                                   
-                    self._refine_ls_shift_su_mean,              
-                    self._refine_diff_density_max,              
-                    self._refine_diff_density_min,              
-                    self._diffrn_reflns_av_unetI_netI,          
-                    self._database_code_depnum_ccdc_archive,    
-                    self._shelx_res_file,                       
-                    ]
-        for key, column in zip(cif_keys, db_columns):
-            try:
-                self.column = cif.cif_data[key]
-            except Exception as e:
-                print(e)
-            
-            
-
-
-
+        self.data = cif.cif_data["data"]
+        self._cell_length_a = get_float(cif.cif_data["_cell_length_a"])
+        self._cell_length_b = get_float(cif.cif_data['_cell_length_b'])
+        self._cell_length_c = get_float(cif.cif_data['_cell_length_c'])
+        self._cell_angle_alpha = get_float(cif.cif_data['_cell_angle_alpha'])
+        self._cell_angle_beta = get_float(cif.cif_data['_cell_angle_beta'])
+        self._cell_angle_gamma = get_float(cif.cif_data['_cell_angle_gamma'])
+        self._cell_volume = get_float(cif.cif_data["_cell_volume"])
+        self._cell_formula_units_Z = get_int(cif.cif_data["_cell_formula_units_Z"])
+        self._space_group_name_H_M_alt = cif.cif_data["_space_group_name_H-M_alt"]
+        self._space_group_name_Hall = cif.cif_data["_space_group_name_Hall"]
+        self._space_group_centring_type = cif.cif_data["_space_group_centring_type"]
+        self._space_group_IT_number = get_int(cif.cif_data["_space_group_IT_number"])
+        self._space_group_crystal_system = cif.cif_data["_space_group_crystal_system"]
+        self._space_group_symop_operation_xyz = cif.cif_data["_space_group_symop_operation_xyz"]
+        self._audit_creation_method = cif.cif_data["_audit_creation_method"]
+        self._chemical_formula_sum = cif.cif_data["_chemical_formula_sum"]
+        self._chemical_formula_weight = cif.cif_data["_chemical_formula_weight"]
+        self._exptl_crystal_description = cif.cif_data["_exptl_crystal_description"]
+        self._exptl_crystal_colour = cif.cif_data["_exptl_crystal_colour"]
+        """
+        self._exptl_crystal_size_max = cif.cif_data["_exptl_crystal_size_max"]
+        self._exptl_crystal_size_mid = cif.cif_data["_exptl_crystal_size_mid"]
+        self._exptl_crystal_size_min = cif.cif_data["_exptl_crystal_size_min"]
+        self._exptl_absorpt_coefficient_mu = cif.cif_data["_exptl_absorpt_coefficient_mu"]
+        self._exptl_absorpt_correction_type = cif.cif_data["_exptl_absorpt_correction_type"]
+        self._diffrn_ambient_temperature = cif.cif_data["_diffrn_ambient_temperature"]
+        self._diffrn_radiation_wavelength = cif.cif_data["_diffrn_radiation_wavelength"]
+        self._diffrn_radiation_type = cif.cif_data["_diffrn_radiation_type"]
+        self._diffrn_source = cif.cif_data["_diffrn_source"]
+        self._diffrn_measurement_device_type = cif.cif_data["_diffrn_measurement_device_type"]
+        self._diffrn_reflns_number = cif.cif_data["_diffrn_reflns_number"]
+        self._diffrn_reflns_av_R_equivalents = cif.cif_data["_diffrn_reflns_av_R_equivalents"]
+        self._diffrn_reflns_theta_min = cif.cif_data["_diffrn_reflns_theta_min"]
+        self._diffrn_reflns_theta_max = cif.cif_data["_diffrn_reflns_theta_max"]
+        self._diffrn_reflns_theta_full = cif.cif_data["_diffrn_reflns_theta_full"]
+        self._diffrn_measured_fraction_theta_max = cif.cif_data["_diffrn_measured_fraction_theta_max"]
+        self._diffrn_measured_fraction_theta_full = cif.cif_data["_diffrn_measured_fraction_theta_full"]
+        self._reflns_number_total = cif.cif_data["_reflns_number_total"]
+        self._reflns_number_gt = cif.cif_data["_reflns_number_gt"]
+        self._reflns_threshold_expression = cif.cif_data["_reflns_threshold_expression"]
+        self._reflns_Friedel_coverage = cif.cif_data["_reflns_Friedel_coverage"]
+        self._computing_structure_solution = cif.cif_data["_computing_structure_solution"]
+        self._computing_structure_refinement = cif.cif_data["_computing_structure_refinement"]
+        self._refine_special_details = cif.cif_data["_refine_special_details"]
+        self._refine_ls_abs_structure_Flack = cif.cif_data["_refine_ls_abs_structure_Flack"]
+        self._refine_ls_structure_factor_coef = cif.cif_data["_refine_ls_structure_factor_coef"]
+        self._refine_ls_weighting_details = cif.cif_data["_refine_ls_weighting_details"]
+        self._refine_ls_number_reflns = cif.cif_data["_refine_ls_number_reflns"]
+        self._refine_ls_number_parameters = cif.cif_data["_refine_ls_number_parameters"]
+        self._refine_ls_number_restraints = cif.cif_data["_refine_ls_number_restraints"]
+        self._refine_ls_R_factor_all = cif.cif_data["_refine_ls_R_factor_all"]
+        self._refine_ls_R_factor_gt = cif.cif_data["_refine_ls_R_factor_gt"]
+        self._refine_ls_wR_factor_ref = cif.cif_data["_refine_ls_wR_factor_ref"]
+        self._refine_ls_wR_factor_gt = cif.cif_data["_refine_ls_wR_factor_gt"]
+        self._refine_ls_goodness_of_fit_ref = cif.cif_data["_refine_ls_goodness_of_fit_ref"]
+        self._refine_ls_restrained_S_all = cif.cif_data["_refine_ls_restrained_S_all"]
+        self._refine_ls_shift_su_max = cif.cif_data["_refine_ls_shift/su_max"]
+        self._refine_ls_shift_su_mean = cif.cif_data["_refine_ls_shift/su_mean"]
+        self._refine_diff_density_max = cif.cif_data["_refine_diff_density_max"]
+        self._refine_diff_density_min = cif.cif_data["_refine_diff_density_min"]
+        self._diffrn_reflns_av_unetI_netI = cif.cif_data["_diffrn_reflns_av_unetI/netI"]
+        self._database_code_depnum_ccdc_archive = cif.cif_data["_database_code_depnum_ccdc_archive"]
+        self._shelx_res_file = cif.cif_data["_shelx_res_file"]"""
 
 
