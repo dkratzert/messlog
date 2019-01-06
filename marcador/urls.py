59cedd91bf07ce47bc6c5896738d52a8b674cafb
Daniel Kratzert <dkratzert@gmx.de>
2019-01-06 21:35:27 +0100

    refractoring and todo

--- marcador/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path, re_path
-
-from marcador import views
-
-urlpatterns = [
-    re_path(r'^user/(?P<username>[-\w]+)/$', views.bookmark_user, name='marcador_bookmark_user'),
-    path('', views.bookmark_list, name='marcador_bookmark_list'),
-]
