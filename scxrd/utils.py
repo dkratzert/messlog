b43d3201b53be0737aa04c96327674789d54df2b
Daniel Kratzert <dkratzert@gmx.de>
2019-01-24 11:11:09 +0100

    Update utils.py

--- scxrd/utils.py
+++ scxrd/utils.py
@@ -9,14 +9,14 @@ def generate_sha256(file):
     Generates a sha256 chcksum from a FileField file handle.
     """
     f = file.open('rb')
-    hash = hashlib.sha3_256()
+    myhash = hashlib.sha3_256()
     if f.multiple_chunks():
         for chunk in f.chunks(chunk_size=64 * 2 ** 10):
-            hash.update(chunk)
+            myhash.update(chunk)
     else:
-        hash.update(f.read())
+        myhash.update(f.read())
     f.close()
-    return hash.hexdigest()
+    return myhash.hexdigest()
 
 
 def frac_to_cart(frac_coord, cell):
@@ -121,24 +121,6 @@ ABSOLUTE_CONFIGURATION_CHOICES = (
 )
 
 # _refine_ls_hydrogen_treatment
-"""
-The data value must be one of the following:
-refall	refined all H-atom parameters
-refxyz	refined H-atom coordinates only
-refU	refined H-atom U's only
-noref	no refinement of H-atom parameters
-constr	H-atom parameters constrained
-hetero	H-atom parameters constrained for H on C, all H-atom parameters refined for H on heteroatoms
-heteroxyz	H-atom parameters constrained for H on C, refined H-atom coordinates only for H on heteroatoms
-heteroU	H-atom parameters constrained for H on C, refined H-atom U's only for H on heteroatoms
-heteronoref	H-atom parameters constrained for H on C, no refinement of H-atom parameters for H on heteroatoms
-hetero-mixed	H-atom parameters constrained for H on C and some heteroatoms, all H-atom parameters refined for H on remaining heteroatoms
-heteroxyz-mixed	H-atom parameters constrained for H on C and some heteroatoms, refined H-atom coordinates only for H on remaining heteroatoms
-heteroU-mixed	H-atom parameters constrained for H on C and some heteroatoms, refined H-atom U's only for H on remaining heteroatoms
-heteronoref-mixed	H-atom parameters constrained for H on C and some heteroatoms, no refinement of H-atom parameters for H on remaining heteroatoms
-mixed	some constrained, some independent
-undef	H-atom parameters not defined
-"""
 REFINE_LS_HYDROGEN_TREATMENT = (
     ('undef', "H-atom parameters not defined"),
     ('mixed', "some constrained, some independent"),
@@ -150,11 +132,20 @@ REFINE_LS_HYDROGEN_TREATMENT = (
     ('hetero', 'H-atom parameters constrained for H on C, all<wbr> H-atom parameters refined for H on heteroatoms'),
     ('heteroxyz', "H-atom parameters constrained for H on C, refined H-atom coordinates only for H on heteroatoms"),
     ('heteroU', "H-atom parameters constrained for H on C, refined H-atom U's only for H on heteroatoms"),
-    ('heteronoref', "H-atom parameters constrained for H on C, no refinement of H-atom parameters for H on heteroatoms"),
-    ('hetero-mixed', "H-atom parameters constrained for H on C and some heteroatoms, refined H-atom coordinates only for H on remaining heteroatoms"),
-    ('heteroxyz-mixed', 'H-atom parameters constrained for H on C and some heteroatoms, refined H-atom coordinates only for H on remaining heteroatoms'),
-    ('heteroU-mixed', "H-atom parameters constrained for H on C and some heteroatoms, refined H-atom U's only for H on remaining heteroatoms"),
-    ('heteronoref-mixed', "H-atom parameters constrained for H on C and some heteroatoms, no refinement of H-atom parameters for H on remaining heteroatoms"),
+    ('heteronoref', "H-atom parameters constrained for H on C, "
+                    "no refinement of H-atom parameters for H on heteroatoms"),
+    ('hetero-mixed',
+     "H-atom parameters constrained for H on C and some heteroatoms, "
+     "refined H-atom coordinates only for H on remaining heteroatoms"),
+    ('heteroxyz-mixed',
+     'H-atom parameters constrained for H on C and some heteroatoms, '
+     'refined H-atom coordinates only for H on remaining heteroatoms'),
+    ('heteroU-mixed',
+     "H-atom parameters constrained for H on C and some heteroatoms, "
+     "refined H-atom U's only for H on remaining heteroatoms"),
+    ('heteronoref-mixed',
+     "H-atom parameters constrained for H on C and some heteroatoms, "
+     "no refinement of H-atom parameters for H on remaining heteroatoms"),
 )
 
 
