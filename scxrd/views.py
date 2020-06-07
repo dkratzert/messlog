c924e547f6d9aac714b18482f178c557b51389e5
Daniel Kratzert <dkratzert@gmx.de>
2020-06-07 18:45:42 +0200

    refractoring

--- scxrd/views.py
+++ scxrd/views.py
@@ -133,37 +133,46 @@ class MoleculeView(LoginRequiredMixin, View):
     """
 
     def post(self, request, *args, **kwargs):
-        molfile = ''
         print('# Molecule request:')
         pprint(request.POST)
         cif_file = request.POST.get('cif_file')
         exp_id = request.POST.get('experiment_id')
         if not cif_file:
             print('Experiment with id {} has no cif file.'.format(exp_id))
-            # Makes structure view blank:
+            # Show a robot where no cif is found:
             robot = make_robot_svg(randstring(), width=300, height=300)
             return HttpResponse(robot[1:])
-        cif = CifContainer(Path(MEDIA_ROOT).joinpath(Path(cif_file)))
         grow = request.POST.get('grow')
+        cif = CifContainer(Path(MEDIA_ROOT).joinpath(Path(cif_file)))
         if cif.atoms_fract:
-            if grow == 'true':
-                sdm = SDM(list(cif.atoms_fract), cif.symmops, cif.cell[:6], centric=cif.is_centrosymm)
-                try:
-                    needsymm = sdm.calc_sdm()
-                    atoms = sdm.packer(sdm, needsymm)
-                except Exception as e:
-                    print('Error in SDM:', e)
-                    return HttpResponse(' ')
-            else:
-                atoms = cif.atoms_orth
-            try:
-                molfile = MolFile(atoms)
-                molfile = molfile.make_mol()
-            except (TypeError, KeyError):
-                print("Error while writing mol file.")
-            return HttpResponse(molfile)
+            return HttpResponse(self.make_molfile(cif, grow))
         print('Cif file with id {} of experiment {} has no atoms!'.format(cif_file, exp_id))
         return HttpResponse('')
+
+    def make_molfile(self, cif: CifContainer, grow: str) -> str:
+        """
+        Returns a mol file with the molecule from the CIF file.
+        :param cif: The CIF object
+        :param grow: wheather to grow or not
+        :return: molfile string
+        """
+        molfile = ' '
+        if grow == 'true':
+            sdm = SDM(list(cif.atoms_fract), cif.symmops, cif.cell[:6], centric=cif.is_centrosymm)
+            try:
+                needsymm = sdm.calc_sdm()
+                atoms = sdm.packer(sdm, needsymm)
+            except Exception as e:
+                print('Error in SDM:', e)
+                return molfile
+        else:
+            atoms = cif.atoms_orth
+        try:
+            molfile = MolFile(atoms)
+            molfile = molfile.make_mol()
+        except (TypeError, KeyError):
+            print("Error while writing mol file.")
+        return molfile
 
     # always reload complete molecule:
     @never_cache
