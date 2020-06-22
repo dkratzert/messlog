808c1f625037947ce71d76e83236a2c516c55130
Daniel Kratzert <dkratzert@gmx.de>
2020-06-22 18:02:54 +0200

    trying to scroll up

--- static/myjs/scxrd.js
+++ static/myjs/scxrd.js
@@ -215,4 +215,10 @@ $(document).ready(function () {
     $(function () {
         $('[data-toggle="popover"]').popover()
     });
+
+    function topFunction() {
+        console.log('foooo');
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
 });
