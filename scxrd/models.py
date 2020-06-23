02408fefef22bbf0580d0e0b7018b5b80529cdad
Daniel Kratzert <dkratzert@gmx.de>
2020-06-23 08:26:23 +0200

    todo

--- scxrd/models.py
+++ scxrd/models.py
@@ -21,6 +21,10 @@ from scxrd.utils import COLOUR_CHOICES, COLOUR_MOD_CHOICES, COLOUR_LUSTRE_COICES
 """
 TODO:
 - While adding a new user, add also Profile data. 
+- "New Experiment page" from "measure a sample" is missing the sum formula and crystal habit and special remarks
+    - rename "experimental special details" in "edit exp"
+- edit experiment: "Solvents used" and "Reaction conditions" must be adapted
+    - svg of molecule is not displayed
 - cif is deleted from experiment when saving again?
 - check checksum for correctness during file upload and download
 - Measurement temperatue to experiment start page -> done?
