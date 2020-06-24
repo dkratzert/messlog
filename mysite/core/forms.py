46d371a0d5450c82f3717e98bd77ca1b1d69165a
Daniel Kratzert <dkratzert@gmx.de>
2020-06-24 08:45:20 +0200

    fixed user creation forms

--- mysite/core/forms.py
+++ mysite/core/forms.py
@@ -1,36 +1,53 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
 
 from scxrd.models import Profile
 
 
-class UserChangeForm(ModelForm):
+class UserForm(UserCreationForm):
+    """The form to create a new user"""
+    email = forms.EmailField(label=_('email address'), required=True)
+    first_name = forms.CharField(label=_('first name'), max_length=30, required=True)
+    last_name = forms.CharField(label=_('last name'), max_length=150, required=True)
+
     class Meta:
         model = User
-        fields = '__all__'
-        # fields = ('first_name', 'last_name',
-        #          'company',)
-        #          'work_group',
-        #          'email_address',
-        #          'street',
-        #          'house_number',
-        #          'phone_number',
-        #          )
-        # field_classes = {'username': UsernameField}
+        field_classes = {'user_form'   : User,
+                         'profile_form': Profile}
+        fields = ('username', 'password1', 'password2', 'first_name', 'last_name', 'email')
+        # fields = ('__all__')
 
 
-class UserForm(ModelForm):
+class UserEditForm(forms.ModelForm):
+    email = forms.EmailField(label=_('email address'), required=True)
+
     class Meta:
         model = User
-        field_classes = {'user_form': User,
+        field_classes = {'user_form'   : User,
                          'profile_form': Profile}
         fields = ('username', 'first_name', 'last_name', 'email')
-        #fields = ('__all__')
+        # fields = ('__all__')
 
 
-class ProfileForm(ModelForm):
+class ProfileNewForm(forms.ModelForm):
+    phone_number = forms.CharField(required=True)
+
     class Meta:
         model = Profile
-        fields = ('company', 'street', 'house_number', 'building', 'town', 'country', 'postal_code',
-                  'phone_number', 'comment')
-        #fields = ('__all__')
+        fields = ('phone_number',
+                  #'company', 'street', 'house_number', 'building', 'town',
+                  #'country', 'postal_code',
+                  'comment')
+        # fields = ('__all__')
+
+
+class ProfileEditForm(forms.ModelForm):
+    phone_number = forms.CharField(required=True)
+
+    class Meta:
+        model = Profile
+        fields = ('phone_number', 'company', 'street', 'house_number', 'building', 'town',
+                  'country', 'postal_code', 'comment')
+        # fields = ('__all__')
--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,30 +1,52 @@
-from pprint import pprint
-
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, UpdateView
 from django.utils.translation import gettext_lazy as _
-from mysite.core.forms import UserChangeForm, ProfileForm, UserForm
+from django.views.generic import TemplateView, CreateView, UpdateView
+
+from mysite.core.forms import UserForm, UserEditForm, ProfileNewForm, ProfileEditForm
 
 
 class SignUp(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('scxrd:index')
+    form_class = UserForm
+    success_url = reverse_lazy('index')
     template_name = 'registration/new_user.html'
+
+    def get(self, request, *args, **kwargs):
+        """
+        Initializing empty Forms:
+        """
+        return render(request, 'registration/new_user.html', {
+            'user_form'   : UserForm(),
+            'profile_form': ProfileNewForm()
+        })
+
+    def post(self, request, *args, **kwargs):
+        """
+        Saving new user.
+        """
+        user_form = UserForm(request.POST)
+        profile_form = ProfileNewForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, _('Your profile was successfully created!'))
+            return redirect('index')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+        return super().post(request, *args, **kwargs)
 
 
 class UserEdit(UpdateView):
-    form_class = UserForm
-    success_url = reverse_lazy('scxrd:index')
+    form_class = UserEditForm
+    success_url = reverse_lazy('index')
     model = User
     template_name = 'registration/edit_user.html'
 
     def post(self, request, *args, **kwargs):
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        user_form = UserEditForm(request.POST, instance=request.user)
+        profile_form = ProfileEditForm(request.POST, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
@@ -35,13 +57,14 @@ class UserEdit(UpdateView):
         return super().post(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        user_form = UserForm(instance=request.user)
-        profile_form = ProfileForm(instance=request.user.profile)
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
 
         return render(request, 'registration/edit_user.html', {
-            'user_form': user_form,
+            'user_form'   : user_form,
             'profile_form': profile_form
         })
+
 
 class OptionsView(TemplateView):
     """
