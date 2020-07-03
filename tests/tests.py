e885087bb67cf4247e5f11120ac299d15dd4a434
Daniel Kratzert <dkratzert@gmx.de>
2020-07-03 22:10:14 +0200

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -277,48 +277,61 @@ class TestViews(DeleteFilesMixin, TestCase):
 class NewExpTest(DeleteFilesMixin, TestCase):
     def setUp(self):
         self.formdata = dict({'Save'                 : 'Save',
-                         'base'                 : '2',
-                         'crystal_colour'       : '6',
-                         'crystal_habit'        : 'needle',
-                         'crystal_size_x'       : '0.13',
-                         'crystal_size_y'       : '0.12',
-                         'crystal_size_z'       : '0.1',
-                         'csrfmiddlewaretoken'  : '4J3vmTSFVd9QxLy7tnu7dCwa5cealpcsvkH1w7kYG3h1cEtKkGqgZxnLwXOinwpb',
-                         'customer'             : '1',
-                         'experiment_name'      : 'TB_VR40_v1b',
-                         'exptl_special_details': 'blub',
-                         'glue'                 : '2',
-                         'machine'              : '1',
-                         'measure_date'         : '2020-07-03 12:53',
-                         'measurement_temp'     : '102',
-                         'number'               : '88',
-                         'prelim_unit_cell'     : '10 10 10 90 90 90',
-                         'resolution'           : '0.77',
-                         'sum_formula'          : 'C3H4O2',
-                         'cif_file_on_disk'     : '',
-                         })
+                              'base'                 : '2',
+                              'crystal_colour'       : '6',
+                              'crystal_habit'        : 'needle',
+                              'crystal_size_x'       : '0.13',
+                              'crystal_size_y'       : '0.12',
+                              'crystal_size_z'       : '0.1',
+                              'csrfmiddlewaretoken'  : '4J3vmTSFVd9QxLy7tnu7dCwa5cealpcsvkH1w7kYG3h1cEtKkGqgZxnLwXOinwpb',
+                              'customer'             : '1',
+                              'experiment_name'      : 'TB_VR40_v1b',
+                              'exptl_special_details': 'blub',
+                              'glue'                 : '2',
+                              'machine'              : '1',
+                              'measure_date'         : '2020-07-03 12:53',
+                              'measurement_temp'     : '102',
+                              'number'               : '88',
+                              'prelim_unit_cell'     : '10 10 10 90 90 90',
+                              'resolution'           : '0.77',
+                              'sum_formula'          : 'C3H4O2',
+                              'cif_file_on_disk'     : 'scxrd/testfiles/p21c.cif',
+                              })
         # self.selenium = webdriver.Chrome()
         super().setUp()
-        user = User.objects.create(username='testuser', email='test@test.com', is_active=True)
+        user = User.objects.create(username='testuser', email='test@test.com', is_active=True, is_superuser=True)
         user.set_password('Test1234!')
         user.save()
         self.user = authenticate(username='testuser', password='Test1234!')
 
     def test_post_experiment(self):
-
-        response = self.client.post(reverse_lazy('scxrd:new_exp'), data=self.formdata, follow=True)
-        self.assertEqual(response.status_code, 200)
+        c = Client()
+        c.login(username='testuser', password='Test1234!')
+        response = c.get(reverse_lazy('scxrd:new_exp'),  follow=True)
+        self.assertEqual(200, response.status_code)
+        print(response.context)
+        self.assertEqual('testuser', str(response.context.get('user')))
+        self.assertEqual(True, response.context.get('render_required_fields'))
+        self.assertEqual(False, response.context.get('render_unmentioned_fields'))
+        #response = c.get(reverse_lazy('scxrd:new_exp'), follow=True)
+        #print(response.redirect_chain)
+        #response = self.client.post(reverse_lazy('scxrd:new_exp'), follow=True, data=self.formdata)
+        #print('#', response.content)
+        #response = self.client.post(reverse_lazy('scxrd:new_exp'), data=self.formdata, follow=True)
+        #self.assertEqual(response.status_code, 200)
 
     def test_form(self):
         form = ExperimentEditForm(self.formdata)
         self.assertEqual({}, form.errors)
         self.assertEqual(True, form.is_valid())
+        print(form.cleaned_data)
 
     def test_invalid_form(self):
         self.formdata['experiment_name'] = ''
         form = ExperimentEditForm(self.formdata)
         self.assertEqual({'experiment_name': ['This field is required.']}, form.errors)
         self.assertEqual(False, form.is_valid())
+
 
 @override_settings(MEDIA_ROOT=MEDIA_ROOT)
 class TestNewSample(DeleteFilesMixin, TestCase):
