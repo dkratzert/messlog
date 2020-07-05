7c3880fae85f01d04ea4a267ca145816914d0c89
Daniel Kratzert <dkratzert@gmx.de>
2020-07-05 12:50:05 +0200

    operators samples list improvend

--- scxrd/templatetags/myfilters.py
+++ scxrd/templatetags/myfilters.py
@@ -25,11 +25,17 @@ def was_refined(experiments) -> bool:
 
 @register.filter
 def was_measured(experiments) -> bool:
-    # if experiments.count() == 0:
-    #    return False
-    #print([x.ciffilemodel.cif_exists for x in experiments.all()])
-    # TODO: Is this right? Or is it always true?
+    try:
+        return any([x.was_measured for x in experiments.all()])
+    except Exception as e:
+        print('Error in "was_measured" filter:', e)
+        return False
+
+
+@register.filter
+def was_deposited(experiments) -> bool:
     try:
         return any([x.ciffilemodel.cif_exists for x in experiments.all()])
-    except Exception:
+    except Exception as es:
+        print('Error in "was_deposited" filter:', e)
         return False
