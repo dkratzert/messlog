7e18f82c80a8aa06780eb15f9d5f58f7b3ab5a1c
Daniel Kratzert <dkratzert@gmx.de>
2021-01-15 14:35:02 +0100

    Create load_initial_db_data.bat

---
+++ load_initial_db_data.bat
@@ -0,0 +1,6 @@
+
+python manage.py loaddata scxrd\fixtures\glue.json
+python manage.py loaddata scxrd\fixtures\machines.json
+python manage.py loaddata scxrd\fixtures\support.json
+python manage.py loaddata scxrd\fixtures\work_group.json
+
